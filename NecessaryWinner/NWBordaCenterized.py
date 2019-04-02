be90112199d88c264413b3077e1cb331e6925ae2
Théo Delemazure <34279251+TheoDlmz@users.noreply.github.com>
2019-04-02 19:01:21 -0400

    new optimized

--- NecessaryWinner/NWBordaCenterized.py
+++ NecessaryWinner/NWBordaCenterized.py
@@ -1,14 +1,56 @@
 import tools
 
 
+def Up2Down_centerized(Up,m,ldown): #m x |Up[u]| -> O(m²)
+    Down = [[] for i in range(m)]
+    for i in range(m):
+        for j in Up[i]:
+            ldown[j] += 1
+            Down[j].append(i)
+    return Down
+       
+       
 def Step1_centerized(dico,Profile,m): #O(nm²)
-    for P in Profile: #n
-        if str(P) in dico.keys():
-            dico[str(P)][0] += 1
+    j = 0
+    test = len(Profile)//10
+    ldown = [0 for i in range(m)]
+    U = []
+    D = []
+    for i in range(test): #n
+        P = Profile[i]
+        s = str(P)
+        if s in dico.keys():
+            dico[s][0] += 1
+            j += 1
+            U.append(dico[s][1])
+            D.append(dico[s][2])
         else:
             u = Order2Up(P,m)
-            d = Up2Down(u,m)
-            dico[str(P)] = [1,u,d]
+            d = Up2Down_centerized(u,m,ldown)
+            dico[s] = [1,u,d]
+            U.append(u)
+            D.append(d)
+    if j >= test//10:
+        for i in range(test,len(Profile)): #n
+            P = Profile[i]
+            s = str(P)
+            if s in dico.keys():
+                dico[s][0] += 1
+            else:
+                j += 1
+                u = Order2Up(P,m)
+                d = Up2Down_centerized(u,m,ldown)
+                dico[s] = [1,u,d]
+        return True,[],[],ldown
+    else:
+        for i in range(test,len(Profile)): #n
+            P = Profile[i]
+            s = str(P)
+            u = Order2Up(P,m)
+            d = Up2Down_centerized(u,m,ldown)
+            U.append(u)
+            D.append(d)
+        return False,U,D,ldown
     
 def Step3_borda_centerized(c,w,dico,m,l=[]): #O(nm)
     Sw = 0
@@ -26,7 +68,7 @@ def Step3_borda_centerized(c,w,dico,m,l=[]): #O(nm)
         l.append(w)
     return (Sw <= Sc)
 
-def Step2_borda_centerized(c,U,D,m): #O(nm²)
+def Step2_borda_centerized(c,dico,m): #O(nm²)
     for w in range(m): #m
         if c != w:
             if not(Step3_borda_centerized(c,w,dico,m)):
@@ -35,28 +77,68 @@ def Step2_borda_centerized(c,U,D,m): #O(nm²)
 
     
     
-def isThereNcW_bordaC(Profile,m): #O(nm²)
-    current = 0
+def isThereNcW_borda_centerized(Profile,m): #O(nm²)
     dico = dict()
-    Step1_centerized(dico,Profile,m)
+    vud,U,D,ldown = Step1_centerized(dico,Profile,m)
+    jtest = 0
+    notfound = True
+    valmax = np.sum([i+1 for i in range(m)])*len(Profile)/m
+    if vud:
+        listcand = [i for i in range(m)]
+        firstcand = 0
+        current = 0
+    else:
+        notNW = []
+        candNW = []
+        max = 0
+        for jtest in range(m):
+            lowest_score = ldown[jtest]
+            if lowest_score >= valmax:
+                notfound = False
+                if lowest_score < max:
+                    notNW.append(jtest)
+                elif lowest_score > max:
+                    notNW += candNW
+                    candNW = [jtest]
+                    max = lowest_score
+                else:
+                    candNW.append(jtest)
+            else:
+                notNW.append(jtest)
+        if notfound:
+            return "There is no co-necessary winner"
+        firstcand = len(notNW)
+        current = firstcand
+        listcand = notNW + candNW
     list_to_test = []
-    for w in range(1,m): 
-        v = Step3_borda_centerized(current,w,dico,m,list_to_test)
+    for w in range(firstcand+1,m):
+        if vud:
+            v = Step3_borda_centerized(listcand[current],listcand[w],dico,m,list_to_test)
+        else:
+            v = Step3_borda(listcand[current],listcand[w],U,D,m,list_to_test)
         if not(v):
             current = w
     i = 0
     for w in range(current):
         i+=1
-        v = Step3_bordaC(current,w,dico,m)
+        if vud:
+            v = Step3_borda_centerized(listcand[current],listcand[w],dico,m)
+        else:
+            v = Step3_borda(listcand[current],w,U,D,m)
         if not(v):
             break
     ncw = []
     if i == current:
         ncw.append(current)
     for w in list_to_test:
-        v = Step2_borda_centerized(w,dico,m)
+        if vud:
+            v = Step2_borda_centerized(listcand[w],dico,m)
+        else:
+            v = Step2_borda(listcand[w],U,D,m)
         if v:
             ncw.append(w)
     if len(ncw) ==0:
         return "There is no co-necessary winner"
-    return "The necessary co-winners are "+str(ncw)
+    return "The necessary co-winners are "+str([listcand[w] for w in ncw])
+            
+
